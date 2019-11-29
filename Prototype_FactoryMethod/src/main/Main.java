364de4e01dc0a3f22bb83fe4563a7f6e56544fad
Maciej Wiercioch <fjh5919@gmail.com>
2019-11-29 17:06:17 +0100

    Change main method

--- Prototype_FactoryMethod/src/main/Main.java
+++ Prototype_FactoryMethod/src/main/Main.java
@@ -1,20 +1,12 @@
 package main;
 
-import electronic_modules.A;
-import electronic_modules.B;
-import electronic_modules.C;
-import electronic_modules.D;
-import electronic_modules_manager.ElectronicModulesManager;
 import factory.ElectronicModulesFactory;
 
 public class Main {
     public static void main(String[] args) {
-        ElectronicModulesManager manager = new ElectronicModulesManager();
-        manager.addElectronicModule("A", new A());
-        manager.addElectronicModule("B", new B());
-        manager.addElectronicModule("C", new C());
-        manager.addElectronicModule("D", new D());
         System.out.println(ElectronicModulesFactory.ElectronicModuleFactory("A"));
-
+        System.out.println(ElectronicModulesFactory.ElectronicModuleFactory("B"));
+        System.out.println(ElectronicModulesFactory.ElectronicModuleFactory("C"));
+        System.out.println(ElectronicModulesFactory.ElectronicModuleFactory("D"));
     }
 }
